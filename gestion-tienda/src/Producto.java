33de52b83dcff8af84ae65e753bd1e9b18fbf373
josealberto.work@gmail.com <josealberto.work@gmail.com@e065f55e-f63c-dcb2-3d7d-527c55d025e6>
2012-03-11 19:19:51 +0000

    Esta es la clase producto, no he modificado absolutamente nada, solo he añadido comentarios y lo he hecho un poco mas estético xD

---
+++ gestion-tienda/src/Producto.java
@@ -0,0 +1,53 @@
+package practica;
+
+public class Producto {
+  
+	private int codigo;
+	private String nombre;
+	private double precio;
+
+//Constructor
+	
+	public Producto(int codigo, String nombre, double precio){
+		
+		this.codigo=codigo;
+		this.nombre=nombre;
+		this.precio=precio;
+	}//Fin constructor
+
+//Metodos de consulta y modificacion del codigo
+	
+	public int get_codigo(){
+		
+		return codigo;
+	}
+	
+	public void set_codigo(int codg){
+		
+		codigo=codg;
+	}
+
+//Metodos de consulta y modificacion del nombre
+	
+	public String get_nombre(){
+		
+		return nombre;
+	}
+	
+	public void set_nombre(String nomb){
+		
+		nombre=nomb;
+	}
+
+//Metodos de consulta y modificacion del precio
+	
+	public double get_precio(){
+		return precio;
+	}
+	
+	public void set_precio(double prec){
+		
+		precio=prec;
+	}
+
+}//Fin Class Producto
