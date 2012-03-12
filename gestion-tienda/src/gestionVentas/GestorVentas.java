8d71c662570142ef72a330503030d439eb321686
josealberto.work@gmail.com <josealberto.work@gmail.com@e065f55e-f63c-dcb2-3d7d-527c55d025e6>
2012-03-12 21:13:51 +0000

    Modificado main y gestior ventas, hacer pedido completo

--- gestion-tienda/src/gestionVentas/GestorVentas.java
+++ gestion-tienda/src/gestionVentas/GestorVentas.java
@@ -9,6 +9,7 @@ public class GestorVentas {
 	private ArrayList<Producto> list_productos;
 	private ProductoDao produ_dao;
 	private String lista = "\n";
+	private ArrayList<Producto> factura=new ArrayList<Producto>();
 
 	public GestorVentas(String fich) throws FileNotFoundException {
 		produ_dao = new ProductoDao(fich);
@@ -21,8 +22,8 @@ public class GestorVentas {
 	
 	public String listar() {
 		for (int i = 0; i < list_productos.size(); i++) {
-			lista = lista + "(" + i + ")" + list_productos.get(i).get_nombre()
-					+ "    " + list_productos.get(i).get_precio() + "ï¿½" + "\n";
+			lista = lista + "(" + i + ") " + list_productos.get(i).get_nombre()
+					+ "    " + list_productos.get(i).get_precio() + "€" + "\n";
 		}
 		return lista;
 	}
@@ -53,5 +54,21 @@ public class GestorVentas {
 
 	public double consultar_precio(int i) {
 		return list_productos.get(i).get_precio();
+	}
+	
+	public void añadir_factura(int i){
+		factura.add(list_productos.get(i));
+	}
+	
+	public double calculo_factura(){
+		double total_factura=0;
+		for(int i=0;i<factura.size();i++){
+			total_factura=total_factura+factura.get(i).get_precio();
+		}
+		return total_factura;
+	}
+	
+	public ArrayList<Producto> get_factura(){
+		return factura;
 	}
 }
