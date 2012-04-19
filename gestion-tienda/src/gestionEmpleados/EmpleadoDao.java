d3131c02c34163a223cd3efe5d0688f95e024bff
sergio.fdz.gar@gmail.com <sergio.fdz.gar@gmail.com@e065f55e-f63c-dcb2-3d7d-527c55d025e6>
2012-04-19 19:06:08 +0000

    Ya no da errores al cargar los ficheros

--- gestion-tienda/src/gestionEmpleados/EmpleadoDao.java
+++ gestion-tienda/src/gestionEmpleados/EmpleadoDao.java
@@ -11,8 +11,8 @@ public class EmpleadoDao {
     private Fichero fichero = null;
 
     // Atributos princiales de los empleados
-    private String nombre;
     private int cod;
+    private String nombre;
     private String password;
     private int nivel;
 
@@ -59,18 +59,21 @@ public class EmpleadoDao {
 	    fichero.leerLinea();
 	    this.password = fichero.leerLinea();
 
+	    fichero.leerLinea();
+	    this.nivel = Integer.parseInt(fichero.leerLinea());
+
 	    // Comprobamos que tipo de empleado es
 	    fichero.leerLinea();
 
 	    if ("nocturno".equals(fichero.leerLinea())) {
 		fichero.leerLinea();
-		int plus = Integer.parseInt(fichero.leerLinea());
+		double plus = Double.parseDouble(fichero.leerLinea());
 		EmpNocturno empleado = new EmpNocturno(cod, nombre, password,
 			nivel, plus);
 		list_empleados.add(i, empleado);
 	    } else {
 		fichero.leerLinea();
-		double retencion = Integer.parseInt(fichero.leerLinea());
+		double retencion = Double.parseDouble(fichero.leerLinea());
 		EmpDiurno empleado = new EmpDiurno(cod, nombre, password,
 			nivel, retencion);
 		list_empleados.add(i, empleado);
